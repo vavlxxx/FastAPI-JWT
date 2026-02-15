6bb0672000a65639ae4e34b5a982038197849c23
vavlxxx <vladimir.nefedov357@gmail.com>
2026-02-15 22:33:27 +0500

    fix: forgot ro remove permission

--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body, Depends, Response
+from fastapi import APIRouter, Body, Response
 
 from src.api.v1.dependencies.auth import UidByAccess, UidByRefresh
 from src.api.v1.dependencies.db import DBDep
@@ -21,7 +21,6 @@ from src.utils.exceptions import (
     UserNotFoundError,
     UserNotFoundHTTPError,
 )
-from src.utils.permissions import IsAdminPermission
 
 router = APIRouter(
     prefix="/auth",
@@ -81,7 +80,6 @@ async def register(
     path="/profile/",
     summary="Получить профиль пользователя",
     responses=AUTH_PROFILE_RESPONSES,
-    dependencies=(Depends(IsAdminPermission()),),
 )
 async def get_profile(
     db: DBDep,
